58e0a6a2b2b7eb69289d1f430947a065228f6fd5
SoSka <TiX0L@SoSiTi.Sru>
2026-05-21 17:21:56 +0900

    For convenience, I added cpp & h. Added some lines of code to cpp & h.
    I knew about this, but it’s my first time implementing it in code lUL ~_~
    
    Для удобства добавил cpp & h.
    Добавил некоторые строчки кода в cpp & h.
    
    Я знал про это, но впервые внедрял в код lUL~_~

---
+++ Google II/Void's.cpp
@@ -0,0 +1,19 @@
+﻿#include "Void's.h"
+
+
+void Пациент(std::string& Назвищо)
+{
+	system("cls");
+	std::cout << Назвищо << ", ";
+}
+
+
+void Сбросс_Ошибки(double& значение)
+{
+	while (!(std::cin >> значение)) {
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "\033[A\033[K";
+	}
+	while (std::cin.get() != '\n');
+}
---
+++ Google II/Void's.h
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cstdlib>
+#include <conio.h>
+#include <vector>
+#include <fstream>
+
+// Войды.
+void Пациент(std::string& Назвищо);
+
+
+void Сбросс_Ошибки(double& значение);
+
+
+
+//Конец Войдов, Начало Основного кода.
